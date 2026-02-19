14a2a81253855f6e6958566b75b166aa32addf8f
EpicLifeDog <105741808+EpicLifeDog@users.noreply.github.com>
2026-02-19 09:35:57 -0800

    Create loadhomepage.js

---
+++ loadhomepage.js
@@ -0,0 +1,10 @@
+if (typeof(Storage) !== "undefined") {
+    var clicks = localStorage.getItem("clickCount");
+    if (clicks !== "undefined") {
+      document.getElementById("clicks").innerHTML = "Clicks: " + clicks;
+    }
+    var cash = localStorage.getItem("cash");
+    if (cash !== "undefined") {
+      document.getElementById("cash").innerHTML = "$" + cash;
+    }
+}
