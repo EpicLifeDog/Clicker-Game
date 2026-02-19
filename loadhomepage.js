3d70dd4c300fe8a12fff2b451a9e8021d0d6b609
EpicLifeDog <105741808+EpicLifeDog@users.noreply.github.com>
2026-02-19 09:53:37 -0800

    Update loadhomepage.js

--- loadhomepage.js
+++ loadhomepage.js
@@ -1,12 +1,18 @@
 window.onload = function() {
     if (typeof(Storage) !== "undefined") {
         var clicks = localStorage.getItem("clickCount");
-        if (clicks !== null) {
-          document.getElementById("clicks").innerHTML = "Clicks: " + clicks;
+        if (clicks !== "undefined" || null) {
+            document.getElementById("clicks").innerHTML = "Clicks: " + clicks;
+        }
+        else {
+            document.getElementById("clicks").innerHTML = "Clicks: 0";
         }
         var cash = localStorage.getItem("cash");
-        if (cash !== null) {
-          document.getElementById("cash").innerHTML = "$" + cash;
+        if (cash !== "undefined" || null) {
+            document.getElementById("cash").innerHTML = "$" + cash;
+        }
+        else {
+            document.getElementById("cash").innerHTML = "$0";
         }
     }
 }
