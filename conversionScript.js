b49a0da80f73f73c201ec4adb0cfd2a14f55b97d
EpicLifeDog <105741808+EpicLifeDog@users.noreply.github.com>
2026-02-23 10:05:51 -0800

    Create conversionScript.js

---
+++ conversionScript.js
@@ -0,0 +1,8 @@
+function convertCashtoClicks() {
+  var x = document.getElementById("clicks");
+  var y = document.getElementById("cash");
+  var amount = document.getElementbyID("CA>CL");
+  if (amount % 10 === 0) {
+    
+  }
+}
