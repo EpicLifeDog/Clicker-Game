f75c58856569708cadb4552f1f2618cb01218dac
EpicLifeDog <105741808+EpicLifeDog@users.noreply.github.com>
2026-02-23 10:27:43 -0800

    Update conversionScript.js

--- conversionScript.js
+++ conversionScript.js
@@ -1,8 +1,39 @@
 function convertCashtoClicks() {
-  var x = document.getElementById("clicks");
-  var y = document.getElementById("cash");
+  var cash = Num(localStorage.getItem("cashCount"));
+  var clicks = Num(localStorage.getItem("clickCount"));
   var amount = document.getElementbyID("CA>CL");
-  if (amount % 10 === 0) {
+  
+  if (cash >= amount) {
+    var x = document.getElementById("clicks");
+    var y = document.getElementById("cash");
     
+    clicks += (amount * 10);
+    cash -= amount;
+
+    x.innerHTML = clicks;
+    y.innerHTML = cash;
+
+    localStorage.setItem = ("cashCount", cash);
+    localStorage.setItem = ("clickCount", clicks);
+  }
+}
+
+function convertClickstoCash() {
+  var amount = document.getElementbyID("CL>CA");
+  var clicks = Num(localStorage.getItem("clickCount"));
+  var cash = Num(localStorage.getItem("cashCount"));
+
+  if (clicks >= amount) {
+    var x = document.getElementById("clicks");
+    var y = document.getElementById("cash");
+    
+    cash += floor(amount / 10);
+    clicks -= amount;
+
+    x.innerHTML = clicks;
+    y.innerHTML = cash;
+
+    localStorage.setItem = ("cashCount", cash);
+    localStorage.setItem = ("clickCount", clicks);
   }
 }
